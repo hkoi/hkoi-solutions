4bd03e6744b763606f4905e8a466bf58c9ebf3de
Tony Wong <microtony@gmail.com>
2019-02-04 16:51:32 +0800

    Add 2018/19 solutions

--- json2md.js
+++ json2md.js
@@ -1,10 +1,10 @@
 fs = require("fs");
 
-process.stdout.write('#HKOI Final Event Solutions\n\n##License for source codes in this repository\n\n');
+process.stdout.write('# HKOI Final Event Solutions\n\n## License for source codes in this repository\n\n');
 process.stdout.write('Creative Commons Attribution-ShareAlike 4.0 International. ');
 process.stdout.write('The full text of the license is available at ');
 process.stdout.write('https://creativecommons.org/licenses/by-sa/4.0/legalcode.');
-process.stdout.write('\n\n##Solutions\n\n');
+process.stdout.write('\n\n## Solutions\n\n');
 
 var json = fs.readFileSync("solutions.json");
 var solutions = JSON.parse(json);
@@ -13,7 +13,7 @@ years.sort();
 var groups = ['senior', 'junior'];
 for (var i = years.length - 1; i >= 0; i--) {
     var year = years[i];
-    process.stdout.write('###' + year + '\n\n| ID | Name | Code | Slides |\n| ------ | ------ | ------ | ------ |\n');
+    process.stdout.write('### ' + year + '\n\n| ID | Name | Code | Slides |\n| ------ | ------ | ------ | ------ |\n');
     for (var group in groups) {
         for (var taskseq in solutions[year][groups[group]]) {
             var task = solutions[year][groups[group]][taskseq];
